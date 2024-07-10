21a60f2113d921ce5391ada669f9c970178b1419
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-10 16:25:10 +0200

    added comments and fixed the spaces on file

--- gradconnect/members/apps.py
+++ gradconnect/members/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
 
 
+# Define the configuration class for the 'members' app
 class MembersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # Set the default type for auto-incrementing primary keys
+    default_auto_field = 'django.db.models.BigAutoField' 
     name = 'members'
     
     def ready(self):
