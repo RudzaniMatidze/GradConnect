33eb3b84d70421d02ef172924855547748e53b73
RudzaniMatidze <rdzmatidze@gmail.com>
2024-07-10 16:21:34 +0200

    added comments to file

--- gradconnect/gradconnect/urls.py
+++ gradconnect/gradconnect/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+# Define URL patterns for the project
 urlpatterns = [
-    path('', include('members.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('', include('members.urls')), # Include URLs from the 'members' app
+    path('admin/', admin.site.urls),  # Admin site URLs
+    path('accounts/', include('django.contrib.auth.urls')) # Authentication URLs provided by Django
 
 ]
